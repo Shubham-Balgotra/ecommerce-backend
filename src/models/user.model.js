9f8de0edebb0fdba79d907ee6f2905dd5b0f315e
Shubham Balgotra <52531006+Shubham-Balgotra@users.noreply.github.com>
2025-08-24 16:30:42 +0530

    Update user.model.js

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,73 +1,5 @@
-// const mongoose = require('mongoose');
-// const { Schema } = mongoose;
-
-// const userSchema = new Schema({
-//   firstName: {
-//     type: String,
-//     required: function () {
-//       return !this.fromGoogle;
-//     },
-//   },
-//   lastName: {
-//     type: String,
-//     required: function () {
-//       return !this.fromGoogle;
-//     },
-//   },
-//   email: {
-//     type: String,
-//     required: true,
-//     unique: true,
-//   },
-//   password: {
-//     type: String,
-//     required: function () {
-//       return !this.fromGoogle;
-//     },
-//   },
-//   fromGoogle: {
-//     type: Boolean,
-//     default: false,
-//   },
-//   role: {
-//     type: String,
-//     enum: ["USER", "ADMIN", "CUSTOMER"], // Add CUSTOMER
-//     default: "USER",
-//   },
-//   mobile: {
-//     type: String,
-//     validate: {
-//       validator: function (v) {
-//         return /^\+?\d{10,15}$/.test(v);
-//       },
-//       message: (props) => `${props.value} is not a valid phone number!`,
-//     },
-//   },
-//   address: [{ type: mongoose.Schema.Types.ObjectId, ref: "address" }],
-//   paymentInformation: [
-//     {
-//       type: String,
-//     },
-//   ],
-//   ratings: [
-//     {
-//       type: mongoose.Schema.Types.ObjectId,
-//       ref: "ratings",
-//     },
-//   ],
-//   reviews: [
-//     {
-//       type: mongoose.Schema.Types.ObjectId,
-//       ref: "reviews",
-//     },
-//   ],
-// });
-
-// const User = mongoose.model("users", userSchema);
-// module.exports = User;
-
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt'); // 👈 Required for hashing
+const bcrypt = require('bcrypt'); //  Required for hashing
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
@@ -132,7 +64,7 @@ const userSchema = new Schema({
   ],
 });
 
-// ✅ Hash password only if NOT fromGoogle and password is modified
+// Hash password only if NOT fromGoogle and password is modified
 userSchema.pre("save", async function (next) {
   if (this.fromGoogle || !this.isModified("password")) return next();
 
